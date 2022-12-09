4d5ddf3d04f08d5b3ba0ce75c9f10fd8c3c12d3f
isewise <iebooker@gmail.com>
2022-12-09 14:45:37 -0800

    Update s3 bucket.tf

--- s3 bucket.tf
+++ s3 bucket.tf
@@ -2,20 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "terraform-cicd-project-${random_integer.bucket_appendix.id}"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 resource "random_integer" "bucket_appendix_2" {
   min = 1
   max = 1000
